985b757e9280c78206e07c34ef274f9fd9fddec4
hermon1 <34695700+hermon1@users.noreply.github.com>
2020-12-02 13:22:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// question 1
+firstVar;
+
+// question 2
+var name = "Hermon";
+
+// question 3
+var number = 10;
+
+// question 4
+var division = 20 / 5;
+
+// question 5
+var pet = "frog";
+
+typeof pet;
+
+// question 6
+var orderHasShipped = true;
+
+// question 7
+if (orderHasShipped === true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// question 8
+for (var count = 0; count <= 9; count = count + 1) {
+  console.log(count);
+}
